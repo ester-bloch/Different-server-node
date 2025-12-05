effa497de994775ccacb28a9797f2301a2e31298
ester-bloch <eb3212846@gmail.com>
2025-12-05 02:37:32 +0200

    Remove Lessons from tracking

--- Lessons/lesson2/fruits.js
+++
@@ -1,9 +0,0 @@
-const list = [
-    { id: 1, desc: 'banana', color: 'yellow', taste: 'sweet' },
-    { id: 2, desc: 'apple', color: 'pink', taste: 'sweet' },
-    { id: 3, desc: 'orange', color: 'orange', taste: 'sour' },
-    { id: 4, desc: 'kiwi', color: 'brown', taste: 'sour' }
-]
-
-// ייצוא ברירת מחדל
-export default list
